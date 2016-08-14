beae687ed589c90911ef2b1d62de2fdbe50ba5ad
Matthew Coppola <mcoppola832@gmail.com>
2016-08-14 18:12:14 -0400

    some tweaks to sizes, a little bit responsive above 900px

--- public/js/home.js
+++ public/js/home.js
@@ -3,6 +3,8 @@ $(function(){
        $panels = $('.side-panel');
 
    $links.click(function() {
+     $links.removeClass('active');
+     $(this).addClass('active');
      $panels.removeClass('active');
      $panels.filter('[data-id="' + $(this).attr('data-id') + '"]').addClass('active');
    });
