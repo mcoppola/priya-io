b6e856b1ab3188237e4c49c7111dca7416c1c5b0
Matthew Coppola <mcoppola832@gmail.com>
2016-08-14 21:35:24 -0400

    more responsive, added close. scroll

--- public/js/home.js
+++ public/js/home.js
@@ -1,11 +1,17 @@
 $(function(){
   var $links = $('.project-link, .title'),
-       $panels = $('.side-panel');
+      $close = $('.side-panel .side-panel__header__close'),
+      $panels = $('.side-panel');
 
    $links.click(function() {
      $links.removeClass('active');
      $(this).addClass('active');
      $panels.removeClass('active');
      $panels.filter('[data-id="' + $(this).attr('data-id') + '"]').addClass('active');
+   });
+   
+   $close.click(function(){
+     $links.removeClass('active');
+     $panels.removeClass('active');
    });
 });
